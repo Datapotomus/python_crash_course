e3728cd2738d6dab550b63316473add3e92efdb8
Matt Therault <matthew.therault@gmail.com>
2018-03-27 10:21:59 -0400

    Try it yourself execercises

---
+++ greetings.py
@@ -0,0 +1,8 @@
+# Directions: Store names in a list called names.
+
+names = ['matt', 'val', 'zoey', 'tessa']
+message = "Hello how are you doing, "
+print(message + names[0].title() + "?")
+print(message + names[1].title() + "?")
+print(message + names[2].title() + "?")
+print(message + names[3].title() + "?")
---
+++ names.py
@@ -0,0 +1,7 @@
+# Directions: Store names in a list called names.
+
+names = ['matt', 'val', 'zoey', 'tessa']
+print(names[0].title())
+print(names[1])
+print(names[2])
+print(names[3])
