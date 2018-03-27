8d1c29f5f8def3c34ad4a38e3bd2b405055d722e
Matt Therault <matthew.therault@gmail.com>
2018-03-27 09:53:46 -0400

    new bike python code

---
+++ bicycles.py
@@ -0,0 +1,6 @@
+# Writing the bcycles python code.
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bike was a " + bicycles[2].title() + "."
+
+print(message)
+print(bicycles[0].title())
