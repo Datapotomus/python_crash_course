6b795361b22d0576e2919be1985e2f63eff6a304
Matt Therault <matthew.therault@gmail.com>
2018-03-27 13:13:36 -0400

    list exercise

---
+++ your_own_list.py
@@ -0,0 +1,7 @@
+# Making your own list of your favorite transportation.
+
+transportation = ['honda', 'toyota', 'ford']
+list_message = ["I would like to own a ", 'I might own a ', 'I probably will never own a ']
+print(list_message[0] + transportation[0].title() + ' motorcycle.')
+print(list_message[1] + transportation[1].title() + ' camry.') 
+print(list_message[2] + transportation[2].title() + '.')
